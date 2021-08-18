0d961db184b8945b05d1a7f18c75489c27e15482
ArshaLizaRoy1 <89081488+ArshaLizaRoy1@users.noreply.github.com>
2021-08-18 12:57:42 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,15 @@
+function makeBlack(){
+    document.body.style.backgroundColor="black";
+}
+
+function makeWhite(){
+    document.body.style.backgroundColor="white";  
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor="green";  
+}
+
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";  
+}
